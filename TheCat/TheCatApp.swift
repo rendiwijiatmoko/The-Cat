8fbe90ff0048ea0c864295432b060b7d0ce791e9
Rendi Wijiatmoko <rendiwijiatmoko97@gmail.com>
2022-01-12 21:11:40 +0700

    Initial Commit

---
+++ TheCat/TheCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheCatApp.swift
+//  TheCat
+//
+//  Created by Rendi Wijiatmoko on 12/01/22.
+//
+
+import SwiftUI
+
+@main
+struct TheCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
