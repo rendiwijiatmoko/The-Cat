42a92ffa7e46ed869e2663b9e8be330e86187bc0
Rendi Wijiatmoko <rendiwijiatmoko97@gmail.com>
2022-01-13 00:10:06 +0700

    search

--- TheCat/View/BreedListView.swift
+++ TheCat/View/BreedListView.swift
@@ -9,10 +9,19 @@ import SwiftUI
 
 struct BreedListView: View {
     let breeds: [Breed]
+    @State private var searchText: String = ""
+    
+    var filteredBreeds: [Breed] {
+        if searchText.count == 0 {
+            return breeds
+        } else {
+            return breeds.filter{$0.name.contains(searchText)}
+        }
+    }
     var body: some View {
         NavigationView {
             List{
-                ForEach(breeds){ breed in
+                ForEach(filteredBreeds){ breed in
                     NavigationLink {
                         BreedDetailView(breed: breed)
                     } label: {
@@ -22,6 +31,7 @@ struct BreedListView: View {
             }
             .listStyle(PlainListStyle())
             .navigationTitle("Find Your Perfect Cat")
+            .searchable(text: $searchText)
         }
     }
 }
